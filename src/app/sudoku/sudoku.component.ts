9903b74d905aeef0835f5ab5c21d72866e5c5276
Md Turin <matkhan01@yahoo.com>
2023-08-30 00:09:07 +0600

    canvas created

--- src/app/sudoku/sudoku.component.ts
+++ src/app/sudoku/sudoku.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sudoku',
   templateUrl: './sudoku.component.html',
-  styleUrls: ['./sudoku.component.scss']
+  styleUrls: ['./sudoku.component.scss'],
 })
 export class SudokuComponent {
   rows: number = 9;
@@ -11,4 +11,21 @@ export class SudokuComponent {
   rowInGroup: number = 3;
   colInGroup: number = 3;
   grid: number[][] = Array(this.rows).fill(Array(this.cols));
+
+  selectTypes: string[] = ['Normal', 'Killer'];
+  selectSizes: string[] = ['4x4', '6x6', '9x9', '12x12', '16x16', '25x25'];
+
+  onTypeSelected(type: string) {}
+
+  onSizeSelected(size: string) {}
+
+  onGenerateClick() {
+    this.grid = Array(this.rows).fill(Array(this.cols));
+  }
+
+  onSolveClick() {}
+
+  onClearClick() {
+    this.grid = Array(this.rows).fill(Array(this.cols));
+  }
 }
